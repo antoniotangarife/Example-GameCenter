bbb16670f4a5683bb7c5075ca25fcb44ed99687e
DaRkD0G <stephan.yannick@me.com>
2015-01-08 20:38:04 +0100

    Commit

--- Example-Game-Center/ViewController.swift
+++ Example-Game-Center/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /******* ADD GameCenter Swift *******/
+    var gameCenter: GameCenter!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        /******* ADD delegate root view controller GameCenter Swift *******/
+        self.gameCenter = GameCenter(rootViewController: self)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var textIDAchievement: UITextField!
 
+    @IBAction func ActionValidAchievement(sender: AnyObject) {
+        self.gameCenter.addProgressToAnAchievement(progress: 100.00,achievementIdentifier: textIDAchievement.text!)
+    }
 
+    @IBAction func ActionOpenGameCenter(sender: AnyObject) {
+        self.gameCenter.showGameCenter()
+    }
+    
+    @IBAction func ActionResetAchievement(sender: AnyObject) {
+        self.gameCenter.resetAllAchievements()
+    }
+  
+    @IBAction func ActionIsFinish(sender: AnyObject) {
+        self.gameCenter.isAchievementFinished(achievementIdentifier: textIDAchievement.text!)
+    }
+    
 }
 
