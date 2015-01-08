539f5ca3b52bf5e9f06a6a9d4cc71ae9e26f4bfa
DaRkD0G <stephan.yannick@me.com>
2015-01-08 20:03:29 +0100

    Initial Commit

---
+++ Example-Game-Center/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example-Game-Center
+//
+//  Created by DaRk-_-D0G on 08/01/2015.
+//  Copyright (c) 2015 DaRk-_-D0G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
